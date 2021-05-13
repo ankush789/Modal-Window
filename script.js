b4371eaa8be0a5da2df295ed48524ef17bad1be1
ankush789 <56834051+ankush789@users.noreply.github.com>
2021-05-14 02:02:48 +0530

    Implementing the logic

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 'use strict';
-const showModalButton = document.querySelector('.show-modal');
+const showModalButton = document.querySelectorAll('.show-modal');
 const closeModalButton = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+//Functionality for all the 3 buttons
+for (let i = 0; i < showModalButton.length; i++) {
+  showModalButton[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+//Close modal button functionality
+closeModalButton.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+//Hiding modal on clicking over the remaining part of window
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
