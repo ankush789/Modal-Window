69113b92925a4b5df19dddda987a919445547452
ankush789 <56834051+ankush789@users.noreply.github.com>
2021-05-14 02:12:04 +0530

    Refactored the code

--- script.js
+++ script.js
@@ -4,20 +4,23 @@ const closeModalButton = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 //Functionality for all the 3 buttons
 for (let i = 0; i < showModalButton.length; i++) {
-  showModalButton[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  showModalButton[i].addEventListener('click', openModal);
 }
+
 //Close modal button functionality
-closeModalButton.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+closeModalButton.addEventListener('click', closeModal);
+
 //Hiding modal on clicking over the remaining part of window
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+overlay.addEventListener('click', closeModal);
